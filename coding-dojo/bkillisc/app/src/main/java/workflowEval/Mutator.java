4d0bcc6513f3e744d1bb423a948283f135b4be62
Benjamin Killisch <bkillisch@gmail.com>
2021-05-31 18:06:23 +0200

    Erster Commit Mineswepper

---
+++ coding-dojo/bkillisc/app/src/main/java/workflowEval/Mutator.java
@@ -0,0 +1,14 @@
+package main.java.workflowEval;
+
+import java.util.ArrayList;
+
+public class Mutator {
+    public Mutator() {
+        //TODO
+    }
+
+    public ArrayList<Mutation> mutateGeneration(ArrayList<Mutation> mutations) {
+        //TODO
+        return null;
+    }
+}
