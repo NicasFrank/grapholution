1bba32c9a66794701c48c2c62cb7c7b17c55aff5
jliebeck <jan.liebeck@stud.htwk-leipzig.de>
2021-05-31 18:50:40 +0200

    implement gui field logic

--- coding-dojo/jliebeck/src/main/java/MweeperField.java
+++ coding-dojo/jliebeck/src/main/java/MweeperField.java
@@ -1,32 +1,4 @@
-public class MweeperField implements mousePerformer {
-    private position    pos;
-    private FieldStatus status          = FieldStatus.HIDE_UNMARKED;
-    private boolean     isBomb          = false;
-    private int         bombsAroundMe   = 0;
-    private JButton     but;
-    private boolean     isTriggered     = false;
 
-    public mweeperField( int x, int y ) {
-        this.pos = new position(x, y);
-        init();
-    }
-    public mweeperField( position p ) {
-        this.pos = p;
-        init();
-    }
-    public void resetField() {
-        status          = FieldStatus.HIDE_UNMARKED;
-        isBomb          = false;
-        bombsAroundMe   = 0;
-        isTriggered     = false;
-        but.setFont(new Font("Arial", Font.BOLD, 13));
-        but.setBackground(Color.LIGHT_GRAY);
-        but.setText(" ");
-        but.setEnabled(true);
-    }
-    public void setBomb() {
-        this.isBomb = true;
-    }
-    public boolean isBomb() {
-        return isBomb;
-    }
+public class MweeperField implements mousePerformer {
+
+}
