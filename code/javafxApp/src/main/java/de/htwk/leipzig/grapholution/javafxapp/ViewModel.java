9408e4d7fd8d7dca60e7e96ac3d7d5b2ef79dfa7
jliebeck <jan.liebeck@stud.htwk-leipzig.de>
2021-06-25 16:20:05 +0200

    neue viewmodel klasse

---
+++ code/javafxApp/src/main/java/de/htwk/leipzig/grapholution/javafxapp/ViewModel.java
@@ -0,0 +1,12 @@
+package de.htwk.leipzig.grapholution.javafxapp;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ViewModel {
+
+    private TextField inputField;
+    private TextField outputField;
+    private Button startButton;
+    
+}
