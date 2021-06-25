500a4ccbfc2e846f169845a9c26dca497a9d7294
jliebeck <jan.liebeck@stud.htwk-leipzig.de>
2021-06-25 16:45:12 +0200

    event handler für button

--- code/javafxApp/src/main/java/de/htwk/leipzig/grapholution/javafxapp/ViewModel.java
+++ code/javafxApp/src/main/java/de/htwk/leipzig/grapholution/javafxapp/ViewModel.java
@@ -1,5 +1,7 @@
 package de.htwk.leipzig.grapholution.javafxapp;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
@@ -8,5 +10,28 @@ public class ViewModel {
     private TextField inputField;
     private TextField outputField;
     private Button startButton;
-    
+
+    public ViewModel(){
+        startButton = new Button();
+        outputField = new TextField();
+        inputField = new TextField();
+    }
+
+    private void isButtonClicked(){
+        startButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                /**Aktion für starten des Hillclimber Algorithmus;
+
+                 */
+                 }
+        });
+    }
+
+    private void handleInput(){
+
+    }
+
+    private void handleOutput(){
+
+    }
 }
