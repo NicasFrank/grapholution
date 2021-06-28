18b05af00ce6edce8fd28596890010f8d44c820b
jliebeck <jan.liebeck@stud.htwk-leipzig.de>
2021-06-28 18:05:05 +0200

    methode producelibrary gelöscht

--- code/javafxApp/src/main/java/de/htwk/leipzig/grapholution/javafxapp/MainApp.java
+++ code/javafxApp/src/main/java/de/htwk/leipzig/grapholution/javafxapp/MainApp.java
@@ -20,24 +20,10 @@ public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
-
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
-
-        String title = produceTitleUsingLibrary();
-        stage.setTitle(title);
         stage.setScene(scene);
         stage.show();
-    }
-    /**
-     * Noch benötigt ? Absprache im späteren Zeitpunkt
-     * @return
-     */
-    private String produceTitleUsingLibrary() {
-        LinkedList tokens;
-        tokens = split(MessageUtils.getMessage());
-        String result = join(tokens);
-        return WordUtils.capitalize(result);
     }
 
     public static void main(String[] args) {
