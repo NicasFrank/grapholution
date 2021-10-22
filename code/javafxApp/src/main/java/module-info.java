f75f534971cd149f797ad084c019cf1ff90fdd4a
jliebeck <jan.liebeck@stud.htwk-leipzig.de>
2021-10-22 12:28:01 +0200

    added new stylesheet for native gui

--- code/javafxApp/src/main/java/module-info.java
+++ code/javafxApp/src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module code.Grapholution.javafxApp.main {
-    requires code.Grapholution.evoLibrary.main;
+    //requires code.Grapholution.evoLibrary.main;
     requires org.apache.commons.text;
-//    requires javafx.controls;
-//    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.base;
     opens de.htwk.leipzig.grapholution.javafxapp to javafx.fxml;
     exports de.htwk.leipzig.grapholution.javafxapp;
 }
