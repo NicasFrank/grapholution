b9edf8b5b4069c91f6760287200d176e86c48ab0
jliebeck <jan.liebeck@stud.htwk-leipzig.de>
2021-10-22 12:28:01 +0200

    added new stylesheet for native gui

--- code/javafxApp/src/main/java/de/htwk/leipzig/grapholution/javafxapp/MainApp.java
+++ code/javafxApp/src/main/java/de/htwk/leipzig/grapholution/javafxapp/MainApp.java
@@ -1,15 +1,10 @@
 package de.htwk.leipzig.grapholution.javafxapp;
 
-import de.htwk.leipzig.grapholution.evolibrary.list.LinkedList;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.apache.commons.text.WordUtils;
-
-import static de.htwk.leipzig.grapholution.evolibrary.utilities.StringUtils.join;
-import static de.htwk.leipzig.grapholution.evolibrary.utilities.StringUtils.split;
 
 public class MainApp extends Application {
     /**
@@ -21,7 +16,7 @@ public class MainApp extends Application {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
         Scene scene = new Scene(root);
-        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+        scene.getStylesheets().add(getClass().getResource("styles_new.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
     }
