3d01e58378f11bed5377e67ffad7dc93f6cd7e43
bkillisc <benjamin_uwe.killisch@stud.htwk-leipzig.de>
2021-10-26 08:22:05 +0000

    feature/createGitLabPipeline

--- code/javafxApp/src/main/java/de/htwk/leipzig/grapholution/javafxapp/FXMLController.java
+++ code/javafxApp/src/main/java/de/htwk/leipzig/grapholution/javafxapp/FXMLController.java
@@ -1,20 +1,20 @@
 package de.htwk.leipzig.grapholution.javafxapp;
 
-import javafx.beans.property.Property;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+//import javafx.beans.property.Property;
+//import javafx.event.ActionEvent;
+//import javafx.fxml.FXML;
+//import javafx.scene.control.Button;
+//import javafx.scene.control.Label;
+//import javafx.scene.control.TextField;
 
 public class FXMLController {
 
-    @FXML
-    private Button startButton;
-    @FXML
-    private TextField inputField;
-    @FXML
-    private TextField outputField;
+//    @FXML
+//    private Button startButton;
+//    @FXML
+//    private TextField inputField;
+//    @FXML
+//    private TextField outputField;
 
     private final ViewModel viewModel = new ViewModel();
 
@@ -23,15 +23,15 @@ public class FXMLController {
      * Verknüpft textProperties mit dem ViewModel
      */
     public void initialize() {
-        inputField.textProperty().bindBidirectional(viewModel.inputFieldProperty());
-        outputField.textProperty().bind(viewModel.outputFieldProperty());
+//        inputField.textProperty().bindBidirectional(viewModel.inputFieldProperty());
+//        outputField.textProperty().bind(viewModel.outputFieldProperty());
     }
 
-    /**
-     *
-     * @param actionEvent: Klick event des Start Buttons
-     */
-    public void sendButtonClick(ActionEvent actionEvent) {
-        viewModel.onButtonClick();
-    }
+//    /**
+//     *
+//     * @param actionEvent: Klick event des Start Buttons
+//     */
+//    public void sendButtonClick(ActionEvent actionEvent) {
+//        viewModel.onButtonClick();
+//    }
 }
--- code/javafxApp/src/main/java/de/htwk/leipzig/grapholution/javafxapp/MainApp.java
+++ code/javafxApp/src/main/java/de/htwk/leipzig/grapholution/javafxapp/MainApp.java
@@ -1,32 +1,37 @@
 package de.htwk.leipzig.grapholution.javafxapp;
 
 import de.htwk.leipzig.grapholution.evolibrary.list.LinkedList;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+//import javafx.application.Application;
+//import javafx.fxml.FXMLLoader;
+//import javafx.scene.Parent;
+//import javafx.scene.Scene;
+//import javafx.stage.Stage;
 import org.apache.commons.text.WordUtils;
 
 import static de.htwk.leipzig.grapholution.evolibrary.utilities.StringUtils.join;
 import static de.htwk.leipzig.grapholution.evolibrary.utilities.StringUtils.split;
 
-public class MainApp extends Application {
+public class MainApp
+//        extends Application
+{
     /**
      * Methode bindet die fxml datei sowie die css Datei in den Startprozess ein
-     * @param stage: Benutzt um Titel und Scene zu setzen
+//     * @param stage: Benutzt um Titel und Scene zu setzen
      * @throws Exception: Wenn Datei nicht gefunden wurde
      */
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
-        Scene scene = new Scene(root);
-        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
-        stage.setScene(scene);
-        stage.show();
+//    @Override
+    public void start(
+//            Stage stage
+    ) throws Exception {
+//        Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
+//        Scene scene = new Scene(root);
+//        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+//        stage.setScene(scene);
+//        stage.show();
     }
 
     public static void main(String[] args) {
-        launch(args);
+
+//        launch(args);
     }
 }
