6ea1d90bd8a5e282b4e45209da551a16b520e120
jliebeck <jan.liebeck@stud.htwk-leipzig.de>
2021-12-10 17:18:53 +0100

    implemented linechart with dummy data

--- code/javafxApp/src/main/java/de/htwk/leipzig/grapholution/javafxapp/SceneControllerResults.java
+++ code/javafxApp/src/main/java/de/htwk/leipzig/grapholution/javafxapp/SceneControllerResults.java
@@ -1,12 +1,11 @@
 package de.htwk.leipzig.grapholution.javafxapp;
-import de.htwk.leipzig.grapholution.evolibrary.algorithms.hillclimber.Hillclimber;
 import de.htwk.leipzig.grapholution.javafxapp.model.HillModel;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.chart.LineChart;
+import javafx.scene.chart.*;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -23,7 +22,7 @@ public class SceneControllerResults extends SceneController implements Initializ
     @FXML
     private TableView<HillModel> tableViewResults;
     @FXML
-    private LineChart lineChartResults;
+    private LineChart<HillModel, String> lineChartResults;
     @FXML
     private TableColumn<HillModel, String> fitness;
 
@@ -42,14 +41,38 @@ public class SceneControllerResults extends SceneController implements Initializ
     public void setTableViewResults() {
         final ObservableList<HillModel> data = FXCollections.observableArrayList(
                 new HillModel(1),
-            new HillModel(4),
-            new HillModel(3),
-            new HillModel(2));
+                new HillModel(4),
+                new HillModel(3),
+                new HillModel(2));
         fitness.setCellValueFactory(new PropertyValueFactory<>("fitness"));
         tableViewResults.setItems(data);
         tableViewResults.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
     }
-    public void setLineChartResults(Hillclimber hillclimber) {
+    public void setLineChartResults() {
+        final CategoryAxis xAxis = new CategoryAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        xAxis.setLabel("Month");
+
+        XYChart.Series series = new XYChart.Series();
+        series.setName("Visuals");
+
+        Axis x = new NumberAxis("Dauer", 0, 8, 1);
+        Axis y = new NumberAxis("Fitness", 0, 150, 10);
+
+        series.getData().add(new XYChart.Data("Jan", 23));
+        series.getData().add(new XYChart.Data("Feb", 14));
+        series.getData().add(new XYChart.Data("Mar", 15));
+        series.getData().add(new XYChart.Data("Apr", 24));
+        series.getData().add(new XYChart.Data("May", 34));
+        series.getData().add(new XYChart.Data("Jun", 36));
+        series.getData().add(new XYChart.Data("Jul", 22));
+        series.getData().add(new XYChart.Data("Aug", 45));
+        series.getData().add(new XYChart.Data("Sep", 43));
+        series.getData().add(new XYChart.Data("Oct", 17));
+        series.getData().add(new XYChart.Data("Nov", 29));
+        series.getData().add(new XYChart.Data("Dec", 25));
+
+        lineChartResults.getData().add(series);
     }
 
     public void loadButtonTableView(ActionEvent e) {
@@ -66,5 +89,6 @@ public class SceneControllerResults extends SceneController implements Initializ
     @FXML
     public void initialize(URL location, ResourceBundle resources) {
         setTableViewResults();
+        setLineChartResults();
     }
 }
