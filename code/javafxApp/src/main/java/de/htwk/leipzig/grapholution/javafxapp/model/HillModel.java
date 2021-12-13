46a4254c04263dd66573289425a302159850759d
jliebeck <jan.liebeck@stud.htwk-leipzig.de>
2021-12-13 13:36:13 +0100

    implemented ViewModel for Hillclimber

--- code/javafxApp/src/main/java/de/htwk/leipzig/grapholution/javafxapp/model/HillModel.java
+++ code/javafxApp/src/main/java/de/htwk/leipzig/grapholution/javafxapp/model/HillModel.java
@@ -1,10 +1,7 @@
 package de.htwk.leipzig.grapholution.javafxapp.model;
 
-import de.htwk.leipzig.grapholution.evolibrary.algorithms.hillclimber.Hillclimber;
 import de.htwk.leipzig.grapholution.evolibrary.statistics.Statistics;
 import javafx.beans.property.SimpleStringProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 
 public class HillModel extends Statistics {
 
@@ -15,8 +12,6 @@ public class HillModel extends Statistics {
         super();
         this.fitness = new SimpleStringProperty(String.valueOf(fitness));
     }
-
-
-
+    
     public String getFitness(){return fitness.get();}
 }
