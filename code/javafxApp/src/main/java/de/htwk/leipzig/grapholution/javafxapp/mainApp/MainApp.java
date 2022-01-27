170331639f02a55b68d482a676affe4697521370
JanEricSchulze <B00467073@studentmail.uws.ac.uk>
2022-01-27 15:16:39 +0100

    kleinschreibung Packages Lib, Strukturierung Packages GUI, messageUtils gelöscht

---
+++ code/javafxApp/src/main/java/de/htwk/leipzig/grapholution/javafxapp/mainApp/MainApp.java
@@ -0,0 +1,27 @@
+package de.htwk.leipzig.grapholution.javafxapp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+    /**
+     * Methode bindet die fxml datei sowie die css Datei in den Startprozess ein
+     * @param stage: Benutzt um Titel und Scene zu setzen
+     * @throws Exception: Wenn Datei nicht gefunden wurde
+     */
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("BasePane.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("styles_new.css").toExternalForm());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
