5668cfcb3d837de685db06c4fc91a599e593e77f
Benjamin Killisch <bkillisch@gmail.com>
2022-01-30 22:06:37 +0100

    Gradle -jar fix

---
+++ code/javafxApp/src/main/java/de/htwk/leipzig/grapholution/javafxapp/MainClass.java
@@ -0,0 +1,11 @@
+package de.htwk.leipzig.grapholution.javafxapp;
+
+/*
+    Workaround für gradle -jar Bug
+ */
+public class MainClass {
+
+    public static void main(String[] args) {
+        MainApp.main(args);
+    }
+}
